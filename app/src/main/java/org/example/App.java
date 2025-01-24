6e6d5cce3ee6f985c57009eeb363633edab135a8
NNR8608220 <n8reyes2006@gmail.com>
2025-01-24 16:46:00 -0600

    Done

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,50 @@
 package org.example;
+import java.util.Scanner;
 
-public class App {
-  public int getInput() {
+public class App 
+{
+  public static String getInput(int lower, int upper) {
     // Implement your function here
+
+    
+    boolean isValid = false;
+    int value = 0;
+    System.out.println("Hello!");
+
+    while (isValid == false)
+    {
+      System.out.println("Please enter a value:");
+      Scanner in = new Scanner(System.in);
+      if (in.hasNextInt())
+      {
+        value = in.nextInt();
+        if (lower <= value && upper >= value)
+        {
+          isValid = true;
+        } else {
+          System.out.println("Error: Value is out of bounds");
+        }
+      } else {
+        System.out.println("Error: Value is not an Int");
+
+      }
+
+      in.close();
+
+      System.out.println();
+
+    }
+
+    String str = String.valueOf(value);
+    return "Your value is " + str;
+
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     // Test your function here in a driver program
+    System.out.println(getInput(-50,50));
+
   }
+
 }
